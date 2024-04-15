be127dad200eaee5788c532d86a9b1e23052a19b
cassielli66 <50680961+54KHandsome@users.noreply.github.com>
2024-04-14 19:45:48 -0500

    Update
    - Updated the readme
    - Added the Entangling_2 to 8Ke.l1i_pref
    - Added the job_submit_1.sh as the example

---
+++ job_submit_1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=serial_job_test   # Job name
+#SBATCH --mail-type=END,FAIL         # Mail Events (NONE,BEGIN,FAIL,END,ALL)
+#SBATCH --mail-user=pan82396610@tamu.edu   # Replace with your email address
+#SBATCH --ntasks=1                   # Run on a single CPU
+#SBATCH --time=12:00:00              # Time limit hh:mm:ss
+#SBATCH --output=serial_test_%j.log  # Standard output and error log
+#SBATCH --qos=grad                   # Change to ugrad for undergrads
+#SBATCH --partition=non-gpu          # This job does not use a GPU
+
+echo "Running client_001.champsimtrace.xz"
+sh ./run_champsim.sh bimodal-Entangling_2Ke-no-no-no-lru-1core 50000000 50000000 client_001.champsimtrace.xz
